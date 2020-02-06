53aa7e1a83e1bffba14f0f75d7c1c807c7e035aa
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-02-06 20:02:21 -0300

    FIX / Altera o arquivo babel config

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: [
     /* altera funcionalidades do JS que o navegador ainda não suporta e.g.: import e export */
-    "babel/preset-env",
+    "@babel/preset-env",
     /* altera as funcionalidades do React que o navegador ainda não suporta */
-    "babel/preset-react"
+    "@babel/preset-react"
   ]
 };
