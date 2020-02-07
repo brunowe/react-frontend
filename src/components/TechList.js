2b8bce6bf474042fdff7b846d5b086e0eec68a76
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-02-07 13:59:34 -0300

    FE-CLIENT / Cria input de newTech e captura o valor digitado pelo usuario

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,15 +3,23 @@ import React, { Component } from "react";
 /* --------------------------------- CONTENT ---------------------------------*/
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "ReactJS", "React Native"]
+  };
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
   };
   render() {
     return (
-      <ul>
-        {this.state.techs.map(tech => (
-          <li key={tech}>{tech}</li>
-        ))}
-      </ul>
+      <>
+        <h1>{this.state.newTech}</h1>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input type="text" onChange={this.handleInputChange} />
+      </>
     );
   }
 }
